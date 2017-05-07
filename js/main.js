5611e480f2b7de74894b3a87ba23c4e987d7fdfd
Pedro Dias <phds@cin.ufpe.br>
2017-05-07 20:24:04 +0200

    finished first rotation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function setGlitching(){
   $(document).ready(function(){
-    var content = $("<div />").append($(".container").clone()).html();
+    let content = $("<div />").append($(".container").clone()).html();
     setInterval(function(){
       setTimeout(function(){
         $(".container").glitch();
@@ -61,4 +61,36 @@ function type(querySelector, str, cb, preStringTyped){
 
 function setupButtons(){
 
+}
+
+function sendData() {
+  let XHR = new XMLHttpRequest();
+  let urlEncodedData = "";
+  let urlEncodedDataPairs = [];
+  let name;
+
+  //random number between 1 and 10
+  let randomNum = Math.floor(Math.random() * 10) + 1;
+  let data= {
+    arg: randomNum
+  }
+  
+  for(name in data) {
+    urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
+  }
+
+  urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
+
+  XHR.addEventListener('load', function(event) {
+    console.log('Yeah! Data sent and response loaded.');
+    console.log(event);
+  });
+
+  XHR.addEventListener('error', function(event) {
+    console.log('Oups! Something goes wrong.');
+  });
+
+  XHR.open('POST', 'https://api.particle.io/v1/devices/2d0040000d47343432313031/led?access_token=dc9f5e2102aa1394e475dc5cffaf991cbb256a05');
+  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  XHR.send(urlEncodedData);
 }
