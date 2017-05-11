353c8c41a3a7b8d33fc92293791787ab3e194f7b
Pedro Dias <phds@cin.ufpe.br>
2017-05-11 22:21:52 +0200

    added music, shitty version

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ function sendData() {
   let data= {
     arg: randomNum
   }
-  
+
   for(name in data) {
     urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(data[name]));
   }
@@ -93,4 +93,85 @@ function sendData() {
   XHR.open('POST', 'https://api.particle.io/v1/devices/2d0040000d47343432313031/led?access_token=dc9f5e2102aa1394e475dc5cffaf991cbb256a05');
   XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   XHR.send(urlEncodedData);
+}
+
+// function setCookie(c_name,value,exdays)
+// {
+//     var exdate=new Date();
+//     exdate.setDate(exdate.getDate() + exdays);
+//     var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+//     document.cookie=c_name + "=" + c_value;
+// }
+//
+// function getCookie(c_name)
+// {
+//     var i,x,y,ARRcookies=document.cookie.split(";");
+//     for (i=0;i<ARRcookies.length;i++)
+//     {
+//       x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+//       y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+//       x=x.replace(/^\s+|\s+$/g,"");
+//       if (x==c_name)
+//         {
+//         return unescape(y);
+//         }
+//       }
+// }
+
+// function setCookie(cname, cvalue, exdays) {
+//   var d = new Date();
+//   d.setTime(d.getTime() + (exdays*24*60*60*1000));
+//   var expires = "expires="+ d.toUTCString();
+//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// }
+//
+// function getCookie(cname) {
+//     var name = cname + "=";
+//     var decodedCookie = decodeURIComponent(document.cookie);
+//     var ca = decodedCookie.split(';');
+//     for(var i = 0; i <ca.length; i++) {
+//         var c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//             c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//             return c.substring(name.length, c.length);
+//         }
+//     }
+//     return "";
+// }
+
+function setCookie(name, value){
+  console.log('setando', name, value)
+  localStorage.setItem(name, value);
+}
+
+function getCookie(name){
+  return localStorage.getItem(name);
+}
+
+function playMusic(){
+  var song = document.getElementsByTagName('audio')[0];
+  var played = false;
+
+  function update() {
+    var tillPlayed = getCookie('timePlayed');
+    song.volume=0.1;
+    if(!played){
+        if(tillPlayed){
+          song.currentTime = tillPlayed;
+          song.play();
+          played = true;
+        }
+        else {
+            song.play();
+            played = true;
+        }
+    }
+    else {
+
+      setCookie('timePlayed', song.currentTime);
+    }
+  }
+  setInterval(update,300);
 }
