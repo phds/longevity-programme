e70794f5f7ac4a0d37b14b83986385ec08b709a3
Pedro Dias <phds@cin.ufpe.br>
2017-05-15 03:52:11 +0200

    finished change for view format

--- js/main.js
+++ js/main.js
@@ -1,30 +1,50 @@
+var intervalId;
+var timeoutId;
 function setGlitching(){
   $(document).ready(function(){
     let content = $("<div />").append($(".container").clone()).html();
-    setInterval(function(){
-      setTimeout(function(){
+    intervalId = setInterval(function(){
+      timeoutId = setTimeout(function(){
         $(".container").glitch();
         setTimeout(function(){
           $('canvas').remove();
           $('body').prepend(content);
         }, 500)
-      }, Math.floor(Math.random() * (3000 - 1000 + 1)) + 2000)
+      }, Math.floor(Math.random() * (3000 - 1000 + 1)) + 2000);
     }, 3100);
   });
 }
 
-function setImmediateGlitch(){
-
+function disableGlitching(){
+  // clearInterval(intervalId);
+  // clearTimeout(timeoutId);
+  var highestTimeoutId = setTimeout(";");
+  for (var i = 0 ; i < highestTimeoutId ; i++) {
+      clearTimeout(i);
+  }
 }
-function setEnterAsNextPage(nextPage, q5){
+
+function setEnterAsNextView(nextPage, finalQuestion){
   let secondaryMessage = document.querySelector('.secondary>span');
   secondaryMessage.classList.add('show');
+
+
   document.onkeypress = function(e){
     if(e.keyCode == 13){
-      document.querySelector('#next').play();
-      timeoutTime = q5 ? 500 : 100;
+      document.onkeypress = undefined;
+      disableGlitching();
+      if(finalQuestion){
+        document.querySelector('#next-final').play();
+        timeoutTime = 700;
+      }
+      else{
+        document.querySelector('#next').play();
+        timeoutTime = 100;
+      }
+
+
       setTimeout(function(){
-        window.location.href = nextPage;
+        setView(nextPage);
       }, timeoutTime);
     }
   }
@@ -34,7 +54,7 @@ function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
-function removeEnterAsNextPage(){
+function removeEnterAsNextView(){
   let secondaryMessage = document.querySelector('.secondary>span');
   secondaryMessage.classList.remove('show');
   document.onkeypress = null;
@@ -63,18 +83,14 @@ function type(querySelector, str, cb, preStringTyped){
   });
 }
 
-function setupButtons(){
-
-}
-
 function sendData() {
   let XHR = new XMLHttpRequest();
   let urlEncodedData = "";
   let urlEncodedDataPairs = [];
   let name;
 
-  //random number between 1 and 10
-  let randomNum = Math.floor(Math.random() * 10) + 1;
+  //random number between 3 and 10
+  let randomNum = Math.floor(Math.random() * 10) + 3;
   let data= {
     arg: randomNum
   }
@@ -99,83 +115,359 @@ function sendData() {
   XHR.send(urlEncodedData);
 }
 
-// function setCookie(c_name,value,exdays)
-// {
-//     var exdate=new Date();
-//     exdate.setDate(exdate.getDate() + exdays);
-//     var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
-//     document.cookie=c_name + "=" + c_value;
-// }
-//
-// function getCookie(c_name)
-// {
-//     var i,x,y,ARRcookies=document.cookie.split(";");
-//     for (i=0;i<ARRcookies.length;i++)
-//     {
-//       x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
-//       y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
-//       x=x.replace(/^\s+|\s+$/g,"");
-//       if (x==c_name)
-//         {
-//         return unescape(y);
-//         }
-//       }
-// }
-
-// function setCookie(cname, cvalue, exdays) {
-//   var d = new Date();
-//   d.setTime(d.getTime() + (exdays*24*60*60*1000));
-//   var expires = "expires="+ d.toUTCString();
-//   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-// }
-//
-// function getCookie(cname) {
-//     var name = cname + "=";
-//     var decodedCookie = decodeURIComponent(document.cookie);
-//     var ca = decodedCookie.split(';');
-//     for(var i = 0; i <ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') {
-//             c = c.substring(1);
-//         }
-//         if (c.indexOf(name) == 0) {
-//             return c.substring(name.length, c.length);
-//         }
-//     }
-//     return "";
-// }
-
-function setCookie(name, value){
-  console.log('setando', name, value)
-  localStorage.setItem(name, value);
+function setView(view){
+  $.ajax({
+   url:view.href,
+   type:'GET',
+   success: function(data){
+     $("body").prepend(data);
+     $("head title").html(view.title);
+     view.js();
+   }
+  });
 }
 
-function getCookie(name){
-  return localStorage.getItem(name);
-}
-
-function playMusic(){
-  var song = document.getElementsByTagName('audio')[0];
-  var played = false;
-
-  function update() {
-    var tillPlayed = getCookie('timePlayed');
-    song.volume=0.0;
-    if(!played){
-        if(tillPlayed){
-          song.currentTime = tillPlayed;
-          song.play();
-          played = true;
-        }
-        else {
-            song.play();
-            played = true;
-        }
+views = {
+  loading:{
+    title: "LOADING",
+    href: "loading.html",
+    js: function(){
+      document.querySelector('#music').volume = 0.1;
+      document.querySelector('#music').play();
+      setTimeout(function(){
+        setView(views.welcome);
+      },6000)
     }
-    else {
+  },
+  welcome:{
+    title: "WELCOME",
+    href: "welcome.html",
+    js: function(){
+      document.querySelector('#welcome').volume = 0.1;
+      document.querySelector('#welcome').play();
+      setTimeout(function(){
+        document.querySelector('.primary>.text').classList.remove('fade-in');
+        document.querySelector('.primary>.text').classList.add('initial');
+        document.querySelector('.primary>.text').classList.remove('show');
 
-      setCookie('timePlayed', song.currentTime);
+        setGlitching();
+      }, 3600);
+      setTimeout(function(){
+        setEnterAsNextView(views.headphones);
+      }, 3000);
     }
+  },
+  headphones:{
+    title: "HEADPHONES",
+    href: "headphones.html",
+    js: function(){
+      setTimeout(function(){
+        document.querySelector('.primary>.text').classList.remove('fade-in');
+        document.querySelector('.primary>.text').classList.add('initial');
+        document.querySelector('.primary>.text').classList.remove('show');
+
+        setGlitching();
+      }, 3600);
+      setTimeout(function(){
+        setEnterAsNextView(views.instructions);
+      }, 3000);
+    }
+  },
+  instructions:{
+    title: "INSTRUCTIONS",
+    href: "instructions.html",
+    js: function(){
+      let str = 'Follow and answer the next 5 questions.<br/><br />Use the keypad and mouse to answer the randomly generated queries.';
+      type('.message>span', str, function(){
+        // let task = pickTask();
+        setEnterAsNextView(views.tasks.task18);
+      })
+    }
+  },
+  finished:{
+    title: "FINISHED",
+    href: "finished.html",
+    js: function(){
+      var str = 'You have now finished a work rotation.^1500<br/><br />Please take your ticket reward.';
+      let secondaryMessage = document.querySelector('.secondary>span');
+      type('.message>span', str, function(){
+        secondaryMessage.classList.add('show');
+        setEnterAsNextView(views.printing);
+      });
+    }
+  },
+  printing:{
+    title: "PRINTING",
+    href: "printing.html",
+    js: function(){
+      document.querySelector('#printing').play();
+      sendData();
+      setTimeout(function(){
+        setView(views.thankyou);
+      }, 8000);
+    }
+  },
+  thankyou:{
+    title: "THANK YOU",
+    href: "thankyou.html",
+    js: function(){
+      setTimeout(function(){
+        document.querySelector('.primary>.text').classList.remove('fade-in');
+        document.querySelector('.primary>.text').classList.add('initial');
+        document.querySelector('.primary>.text').classList.remove('show');
+
+        setTimeout(function(){
+          document.querySelector('#music').pause();
+
+          $(".container").glitch();
+          document.querySelector('#thankyou').play();
+          setTimeout(function(){
+            window.location.href = 'index.html';
+          }, 1400)
+        },3500)
+
+      }, 2000);
+    }
+  },
+  tasks:{
+    task2:{
+      title: "COW",
+      href: "tasks/task2.html",
+      js: function(){
+        let input = document.querySelector(".input__field");
+        let el = document.querySelector('.cow');
+
+        type('.title>span', 'Double click this icon.', function(){
+          el.classList.remove('hide');
+        });
+
+        el.ondblclick = function(e){
+
+          el.children[0].src = "../assets/cow-full.svg";
+          el.classList.add('filled');
+          document.querySelector('.typed-cursor').style.visibility = 'hidden';
+          setTimeout(function(){
+            setEnterAsNextView(views.tasks.task12);
+          }, 1200);
+        }
+
+        el.onfocus = function(){
+
+          if(dateValid) {
+            input.classList.add('valid');
+          }
+        }
+
+      }
+    },
+    task3:{
+      title: "HEART",
+      href: "tasks/task3.html",
+      js: function(){
+        let input = document.querySelector(".input__field");
+        let el = document.querySelector('.heart');
+
+        type('.title>span', 'Double click this icon.', function(){
+          el.classList.remove('hide');
+        });
+
+        el.ondblclick = function(e){
+
+          el.children[0].src = "../assets/heart-full.svg";
+          el.classList.add('filled');
+          document.querySelector('.typed-cursor').style.visibility = 'hidden';
+          setTimeout(function(){
+            setEnterAsNextView(views.tasks.task16);
+          }, 1200);
+        }
+
+      }
+    },
+    task6:{
+      title: "HUNGRINESS",
+      href: "tasks/task6.html",
+      js: function(){
+        let el = document.querySelector('.btn-group');
+        type('.title>span', 'On a scale from 1-10, rate your hunger level, 10 being<br />the hungriest.', function(){
+            el.classList.remove('hide');
+        });
+
+
+        let buttons = document.querySelectorAll('.btn-group div');
+        for (var i = 0; i < buttons.length; i++) {
+          buttons[i].addEventListener('click',function(e){
+            let cursor = document.querySelector('.typed-cursor');
+            if(cursor){
+              cursor.remove();
+            }
+            for (var i = 0; i < buttons.length; i++) {
+              buttons[i].classList.remove('active');
+            }
+            e.target.classList.add('active');
+            setTimeout(function(){
+              setEnterAsNextView(views.tasks.task16);
+            }, 1700);
+          });
+        }
+
+      }
+    },
+    task12:{
+      title: "DATE",
+      href: "tasks/task12.html",
+      js: function(){
+        let dateValid = false;
+        let input = document.querySelector(".input__field");
+        let el = document.getElementById('task-input');
+
+        type('.title>span', 'Enter the DD/MM of your birth date.', function(){
+          el.parentNode.classList.remove('hide');
+        });
+
+        el.onkeypress = function(e){
+
+          var charCode = e.which || e.keyCode;
+          var charStr = String.fromCharCode(charCode);
+          if (!/[0-9]/.test(charStr) || el.value.length > 4) {
+              return false;
+          }
+
+        }
+        el.onkeyup = function(e){
+          if(el.value.length == 2 && e.keyCode != 8){
+            el.value = el.value + '/';
+          }
+          else{
+            let day = el.value.split("/")[0];
+            let month = el.value.split("/")[1];
+            dateValid = day <= 31 && month <= 12 && month.length == 2;
+            let secondaryMessage = document.querySelector('.secondary>span');
+            if(dateValid){
+              input.classList.add('valid');
+              el.onkeyup = undefined;
+              setEnterAsNextView(views.tasks.task3);
+            }
+            else {
+              input.classList.remove('valid');
+              removeEnterAsNextView();
+            }
+          }
+        }
+
+        function insertAfter(newNode, referenceNode) {
+            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        }
+
+        let cursor = document.querySelector('.typed-cursor').cloneNode();
+        el.onfocus = function(){
+          document.querySelector('.typed-cursor').style.visibility = 'hidden';
+          if(dateValid) {
+            input.classList.add('valid');
+          }
+        }
+
+        el.onblur = function(){
+          document.querySelector('.typed-cursor').style.visibility = 'visible';
+          input.classList.remove('valid');
+        }
+      }
+    },
+    task16:{
+      title: "LOCATION",
+      href: "tasks/task16.html",
+      js: function(){
+        let validInput = false;
+        let timeoutId;
+        let input = document.querySelector(".input__field");
+        let el = document.getElementById('task-input');
+
+        type('.title>span', 'Enter your current location.', function(){
+          el.parentNode.classList.remove('hide');
+        });
+
+
+
+        el.onkeypress = function(e){
+          if(el.value.length > 5){
+            el.onkeypress = undefined;
+            setEnterAsNextView(views.finished, true);
+          }
+        }
+
+
+        function insertAfter(newNode, referenceNode) {
+            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        }
+
+        let cursor = document.querySelector('.typed-cursor').cloneNode();
+        el.onfocus = function(){
+          document.querySelector('.typed-cursor').style.visibility = 'hidden';
+          if(validInput) {
+            input.classList.add('valid');
+          }
+        }
+
+        el.onblur = function(){
+          document.querySelector('.typed-cursor').style.visibility = 'visible';
+          input.classList.remove('valid');
+        }
+      }
+    },
+    task18:{
+      title: "NUMBERS",
+      href: "tasks/task18.html",
+      js: function(){
+        let validInput = false;
+        let input = document.querySelector(".input__field");
+        let el = document.getElementById('task-input');
+
+        type('.title>span', 'Enter 5 random digits.', function(){
+          el.parentNode.classList.remove('hide');
+        });
+
+        el.onkeypress = function(e){
+
+          var charCode = e.which || e.keyCode;
+          var charStr = String.fromCharCode(charCode);
+          if (!/[0-9]/.test(charStr) || el.value.length > 4) {
+              return false;
+          }
+
+        }
+        el.onkeyup = function(e){
+
+          let numbers = el.value;
+          validInput = numbers.length == 5;
+          if(validInput){
+            input.classList.add('valid');
+            el.onkeyup = undefined;
+            setEnterAsNextView(views.tasks.task2);
+          }
+          else {
+            input.classList.remove('valid');
+            removeEnterAsNextView();
+          }
+
+        }
+
+        function insertAfter(newNode, referenceNode) {
+            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        }
+
+        let cursor = document.querySelector('.typed-cursor').cloneNode();
+        el.onfocus = function(){
+          document.querySelector('.typed-cursor').style.visibility = 'hidden';
+          if(validInput) {
+            input.classList.add('valid');
+          }
+        }
+
+        el.onblur = function(){
+          document.querySelector('.typed-cursor').style.visibility = 'visible';
+          input.classList.remove('valid');
+        }
+
+      }
+    },
   }
-  setInterval(update,300);
 }
